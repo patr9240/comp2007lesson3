de5f636661a8e621ba426c0f3bc8a46a71208b24
patr9240 <patr9240@gmail.com>
2016-05-26 18:06:02 -0400

    Added navbar usercontrol

--- comp2007Lesson3/Site.Master.designer.cs
+++ comp2007Lesson3/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace comp2007Lesson3 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// MainNav control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::comp2007Lesson3.NavBar MainNav;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
---
+++ comp2007Lesson3/User_Controls/NavBar.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ * @Author: Patrick Ross
+ * @Date" May 26 2016
+ * @version: 0.0.1 - added SetActivePage method
+ * */
+namespace comp2007Lesson3
+{
+    public partial class NavBar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        /**
+         * 
+         * */
+
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "HomePage":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "ContactPage":
+                    home.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
